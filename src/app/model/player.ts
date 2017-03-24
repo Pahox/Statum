95ba98c82d3aa388d7d23f8f082dcada7e31e4c2
TapSInSpace <s.marchetti1312@gmail.com>
2017-03-24 17:40:46 +0100

    Projet v1 + ajout ressources

---
+++ src/app/model/player.ts
@@ -0,0 +1,16 @@
+export class Player {
+    name;
+    roll_cofee;
+    roll_water;
+    roll_croco;
+    roll_base;
+    roll_other;
+    
+    win_cofee;
+    win_water;
+    win_croco;
+    win_base;
+    win_other;
+    
+    winrate;
+}
